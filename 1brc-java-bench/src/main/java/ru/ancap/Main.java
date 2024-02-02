c10fc3547b7220989cdf1cbc95c06a2e54c5a541
javach-kun <108641400+javach-kun@users.noreply.github.com>
2024-02-02 21:36:47 +0500

    1brc final update

--- 1brc-java-bench/src/main/java/ru/ancap/Main.java
+++ 1brc-java-bench/src/main/java/ru/ancap/Main.java
@@ -6,7 +6,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Method;
 
 void main(String[] args) throws Throwable {
-    Class<?> class_ = Class.forName(STR."ru.ancap._1brc.\{args[ 1 ]}.CalculateAverage");
+    Class<?> class_ = Class.forName(STR."ru.ancap._1brc.\{args[1]}.CalculateAverage");
     Method method = class_.getMethod("main", String[].class);
     method.setAccessible(true);
     for (int i = 0; i < 10; i++) {
